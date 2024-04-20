a3ef1c193ea9d513e1f421376b0ce80901952aeb
unknown <hankishbawa17@gmail.com>
2024-04-20 13:26:01 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1",{
+    id : "heading"
+},"welcome to the Namste React Course and this is the best react course now to learn !!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading)
